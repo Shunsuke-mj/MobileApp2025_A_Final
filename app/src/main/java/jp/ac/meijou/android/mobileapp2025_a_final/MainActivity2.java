457f6ddd8b08bec249b062ffb473fbfe98b1dba5
Shunsuke-mj <183798932+Shunsuke-mj@users.noreply.github.com>
2025-09-05 15:00:44 +0900

    MainActivity2.javaにGPSを使用して緯度と経度を取得する機能を実装

--- app/src/main/java/jp/ac/meijou/android/mobileapp2025_a_final/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/mobileapp2025_a_final/MainActivity2.java
@@ -1,14 +1,28 @@
 package jp.ac.meijou.android.mobileapp2025_a_final;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private double latitude = 0;    //緯度
+    private double longitude = 0;   //経度
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,28 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                                TextView text4 = findViewById(R.id.textView);
+                                TextView text5 = findViewById(R.id.textView2);
+                                text4.setText(String.valueOf(latitude));
+                                text5.setText(String.valueOf(longitude));
+                            }
+                        }
+                    });
+        } else {
+            //権限がない場合
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1000);
+        }
     }
 }
