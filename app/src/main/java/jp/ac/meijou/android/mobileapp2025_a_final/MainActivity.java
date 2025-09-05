a4561f810c06c0c895d14f73292691ecdf1dc795
Shunsuke-mj <183798932+Shunsuke-mj@users.noreply.github.com>
2025-09-05 13:55:13 +0900

    MainActivity.javaにGPSによる緯度・経度の取得を実装（動作しなかった）https://akira-watson.com/android/gps.html#3

--- app/src/main/java/jp/ac/meijou/android/mobileapp2025_a_final/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/mobileapp2025_a_final/MainActivity.java
@@ -1,14 +1,44 @@
 package jp.ac.meijou.android.mobileapp2025_a_final;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    LocationManager locationManager;
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            isGranted -> {
+                if (isGranted) {
+                    locationStart();
+                }
+                else {
+                    Toast toast = Toast.makeText(this,
+                            "これ以上なにもできません", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +50,46 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION);
+        } else {
+            locationStart();
+        }
+    }
+
+    private void locationStart(){
+        Log.d("debug", "locationStart()");
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if(locationManager != null && locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Log.d("debug", "location manager Enabled");
+        } else {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+            Log.d("debug", "notgpsEnable, startActivity");
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+            Log.d("debug", "checkSelPermission false");
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                1000, 50, (LocationListener) this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView textView1 = findViewById(R.id.textView);
+        String str1 = "Latitude:"+location.getLatitude();
+        textView1.setText(str1);
+
+        TextView textView2 = findViewById(R.id.textView2);
+        String str2 = "Longitude:"+location.getLongitude();
+        textView2.setText(str2);
     }
 }
